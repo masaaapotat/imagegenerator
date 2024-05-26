4a73e37932e0738c43892203f2fd399ec5851aee
masaapotat <masaakavulanya@gmail.com>
2024-05-26 20:53:06 +0300

    adding the heading and the  image

--- src/Components/ImageGenerator/ImageGenerator.js
+++ src/Components/ImageGenerator/ImageGenerator.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './ImageGenerator.css'
+import default_image from '../Assets/default_image.svg'
 
 const ImageGenerator = () => {
   return (
-    <div>ImageGenerator</div>
+    <div className='ai-image-generator'>
+      <div className='header'>Ai <span>ImageGenerator</span></div>
+      <div>Image Loading</div>
+      <div className='image'>
+        <img src={default_image} alt='image'/>
+      </div>
+    </div>
   )
 }
 
