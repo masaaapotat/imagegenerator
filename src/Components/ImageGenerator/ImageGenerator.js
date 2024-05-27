c5f88437503d84384c7116fa30eb8430619bf22c
masaapotat <masaakavulanya@gmail.com>
2024-05-27 06:12:41 +0300

    styling the search input

--- src/Components/ImageGenerator/ImageGenerator.js
+++ src/Components/ImageGenerator/ImageGenerator.js
@@ -6,9 +6,13 @@ const ImageGenerator = () => {
   return (
     <div className='ai-image-generator'>
       <div className='header'>Ai <span>ImageGenerator</span></div>
-      <div>Image Loading</div>
+      <div className='image-loading'></div>
       <div className='image'>
         <img src={default_image} alt='image'/>
+      </div>
+      <div className='search-box'>
+        <input type='text' className='search-input' placeholder='Describe what you want the AI to create'/>
+        <div className="generate-button">Generate</div>
       </div>
     </div>
   )
